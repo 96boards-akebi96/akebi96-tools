4880f24d473866b387afc33e614208c5d88c019a
Masami Hiramatsu <masami.hiramatsu@linaro.org>
2019-03-22 19:38:44 +0900

    minicom: Expand parameter to get -c config and firmware install
    Expand minicom script parameter to get -c option and "firmware" to
    install firmware. IOW, it doesn't install firmware by default.
    
    Signed-off-by: Masami Hiramatsu <masami.hiramatsu@linaro.org>

--- minicom/akebi96-tftp-aosp.sh
+++ minicom/akebi96-tftp-aosp.sh
@@ -6,10 +6,11 @@
 # This program is released under the MIT License, see LICENSE.
 
 usage(){
-  echo "Usage: $0 [CONFIG_FILE]"
+  echo "Usage: $0 [-c CONFIG_FILE] [firmware]"
   exit 0
 }
 
+FWINST=0
 SERVER_IP=192.168.11.1
 BOARD_IP=192.168.11.10
 GATEWAY_IP=192.168.11.1
@@ -17,10 +18,27 @@ NETMASK=255.255.255.0
 LOGFILE=akebi96-install.log
 MINICOM_OPT="" # you may need "-D devicefile"
 
-if [ -f "$1" ]; then
-  . $1
+CONFIG=
+
+while [ $# -ne 0 ]; do
+  case $1 in
+  -c) CONFIG=$2 ; shift 2;;
+  firmware) FWINST=1; shift 1;;
+  *) usage ;;
+  esac
+done
+
+if [ -n "$CONFIG" -a -f "$CONFIG" ]; then
+  . $CONFIG
 fi
 
+if [ $FWINST -ne 0 ]; then
+  INITCNT=0	# Install AOSP and firmware
+else
+  INITCNT=1	# Install AOSP only
+fi
+
+export INITCNT
 export SERVER_IP
 export BOARD_IP
 export NETMASK
